9bf8f9eef708efa042f0e1b649956fbc5e565685
Amarjeet <amarmohanty06112003@gmail.com>
2023-04-11 01:50:53 +0530

    made the voting page

--- pages/vote.js
+++ pages/vote.js
@@ -11,7 +11,33 @@ const Vote = () => {
     })
     return (
         <div>
-            
+
+
+      <main>
+        <h1>Vote</h1>
+
+        <form>
+
+
+          <div>
+            <label htmlFor="option1">Option 1</label>
+            <input type="radio" id="option1" name="vote" value="option1" />
+          </div>
+
+          <div>
+            <label htmlFor="option2">Option 2</label>
+            <input type="radio" id="option2" name="vote" value="option2" />
+          </div>
+
+          <div>
+            <label htmlFor="option3">Option 3</label>
+            <input type="radio" id="option3" name="vote" value="option3" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+
+      </main>           
         </div>
     );
 }
