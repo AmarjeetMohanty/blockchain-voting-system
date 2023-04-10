ad693619d03fba2e9c16dc064fc2a7236b60acb0
Amarjeet <amarmohanty06112003@gmail.com>
2023-04-11 01:19:18 +0530

    400 changed to 404 error

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
                 return res.status(200).json({ success: true, data: user })
             }
             else{
-                return res.status(400).json({ success: false })
+                return res.status(404).json({ success: false })
             }
         })
       } catch (error) {
