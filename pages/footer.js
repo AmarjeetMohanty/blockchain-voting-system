c37619590c80eab68bce1d219eea06dddf81423c
Amarjeet <amarmohanty06112003@gmail.com>
2023-04-13 01:41:04 +0530

    added stats page

--- pages/footer.js
+++ pages/footer.js
@@ -2,12 +2,12 @@ import { Box } from "@mui/material"
 import axios from "axios"
 import React from "react"
 import { useEffect } from "react"
-
+import { useRouter } from 'next/router'
 const Footer = () => {
-    
+    const router = useRouter()
     useEffect(()=>{
         const user_id = localStorage.getItem("user")
-        
+
         if (!user_id){
             router.push("/verification")
         }else{
@@ -28,13 +28,37 @@ const Footer = () => {
         })}
         }
     },[])
+    const handleSubmit = async (event)=>{
+      event.preventDefault();
+      router.push("/stats");
+    }
+
     return (<Box sx={{
         height:"100vh",
         display:"flex",
         justifyContent:"center",
         alignItems:"center"
     }}>
+      
+      <Box sx={{
+      height:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+  }}>
         <h2>Thanks for Voting</h2>
+        </Box>
+        <Box sx={{
+        height:"100vh",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+    }}>
+     <button onClick={handleSubmit}>View Statistics</button>
+
+    </Box>
+
+
         </Box>)
 }
 
