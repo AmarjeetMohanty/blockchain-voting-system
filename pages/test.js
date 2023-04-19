898da3b72a24dc1f0f164b4c1461bb91ea5472b1
Amarjeet <amarmohanty06112003@gmail.com>
2023-04-19 23:28:37 +0530

    updated abi

--- pages/test.js
+++ pages/test.js
@@ -2,14 +2,14 @@ const { ethers } = require("ethers")
 const fs = require('fs');
 const jsonFile = "./abi.json";
 const abi=JSON.parse(fs.readFileSync(jsonFile));
-const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138'; // Insert contract address here
+const contractAddress = '0x1aa9407865c76ea7203cf74263db7d616c10e56115f1e04d1112796869859460'; // Insert contract address here
 
 
 async function main(){
-	//let provider = new ethers.getDefaultProvider("https://mainnet.infura.io/v3/59bb994837e348dcb3b79612153b46bd", {})
-	const provider = new ethers.providers.Web3Provider(window.ethereum)
+	let provider = new ethers.getDefaultProvider("https://mainnet.infura.io/v3/59bb994837e348dcb3b79612153b46bd", {})
+	//const provider = new ethers.providers.Web3Provider(window.ethereum)
     const contract = new ethers.Contract(contractAddress,abi, provider)
-    const name = await contract.winningName()
+    const name = await contract.getAddress
     console.log(name)
 	
     const block = await provider.getBlockNumber();
